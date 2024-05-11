deb940ccb69ddefa1af53885008606d0694264cb
jupitters <83190205+jupitters@users.noreply.github.com>
2024-05-10 21:32:29 -0300

    Update 3_Tabuada.asm

--- 3_Tabuada.asm
+++ 3_Tabuada.asm
@@ -1,41 +1,41 @@
 .data
-prompt: .asciiz "Digite um número para a tabuada: "
+prompt: .asciiz "Digite um nÃºmero para a tabuada: "
 espaco: .asciiz " "
 
 .text
     li $t4, 1           # Inicializa o multiplicador como 1
     li $t5, 10		# Final do multiplicador como 10
 
-    li $v0, 4           # Usuário digitar um numero
+    li $v0, 4           # UsuÃ¡rio digitar um numero
     la $a0, prompt      
     syscall
 
-    # Recebe o número do usuário
+    # Recebe o nÃºmero do usuÃ¡rio
     li $v0, 5           # Syscall para ler inteiro
     syscall
     
-    move $t1, $v0    # Move o número para $t1
-    move $a0, $t1    # Move o número para $a0
+    move $t1, $v0    # Move o nÃºmero para $t1
+    move $a0, $t1    # Move o nÃºmero para $a0
     
     inicio:
+    move $a0, $t1    # Move o nÃºmero para $a0
     jal multiplica
     bgt $t4, $t5, saida
+    
+    li $v0, 4           # Syscall para imprimir um espaco
+    la $a0, espaco
+    syscall
     j inicio
     
     
 multiplica:
     # Calcula e imprime o valor da tabuada
-    move $a0, $t1    # Move o número para $a0
-    mul $a0, $a0, $t4   # Calcula o valor da tabuada (número * multiplicador)
+    mul $a0, $a0, $t4   # Calcula o valor da tabuada (nÃºmero * multiplicador)
     li $v0, 1           # Syscall para imprimir inteiro
     syscall
 
     # Incrementa o multiplicador
     addi $t4, $t4, 1
-
-    li $v0, 4           # Syscall para imprimir um espaco
-    la $a0, espaco
-    syscall
     
     jr $ra 
 
